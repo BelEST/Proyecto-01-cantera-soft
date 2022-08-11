6e54226277ef56c5e927b11f441620e1e43f721a
ebell <ebellhen@gmail.com>
2022-08-11 20:29:14 -0300

    4funciones y variables hechas

---
+++ ‘algoritmos.js
@@ -0,0 +1,39 @@
+function suma() {
+let x = 10;
+let y = 5;
+
+let suma= x + y ;
+return console.log (suma);
+
+}
+
+suma()
+
+function resta() {
+let a = 6;
+let b = 3;
+let resta= a-b;
+return console.log (resta)
+
+}
+
+resta()
+
+function multi() {
+let c = 8;
+let d = 2;
+let multi= c*d;
+return console.log (multi)
+
+}
+
+multi()
+
+function division() {
+    let r = 7;
+    let t = 2;
+    let division= r/t ;
+    return console.log (division)
+}
+
+division()
