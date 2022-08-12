cae719d178eb08728ac58f57bda53a2967b55575
ebell <ebellhen@gmail.com>
2022-08-12 15:44:36 -0300

    variables arreglo

--- ‘algoritmos.js
+++ ‘algoritmos.js
@@ -1,39 +1,50 @@
 function suma() {
-let x = 10;
-let y = 5;
+    let x = 10;
+    let y = 5;
+    
+    let suma= x + y ;
+    return console.log (suma);
+    
+    }
+    
+    suma()
+    
+    function resta() {
+    let a = 6;
+    let b = 3;
+    let resta= a-b;
+    return console.log (resta)
+    
+    }
+    
+    resta()
+    
+    function multi() {
+    let c = 8;
+    let d = 2;
+    let multi= c*d;
+    return console.log (multi)
+    
+    }
+    
+    multi()
+    
+    function division() {
+        let r = 7;
+        let t = 2;
+        let division= r/t ;
+        return console.log (division)
+    }
+    
+    division()
 
-let suma= x + y ;
-return console.log (suma);
+     function suma2() {
+        let q = 10;
+        let w = 3;
+        let suma2= q + w ;
+        return console.log (suma2)
+    }
+    suma2()
 
-}
 
-suma()
-
-function resta() {
-let a = 6;
-let b = 3;
-let resta= a-b;
-return console.log (resta)
-
-}
-
-resta()
-
-function multi() {
-let c = 8;
-let d = 2;
-let multi= c*d;
-return console.log (multi)
-
-}
-
-multi()
-
-function division() {
-    let r = 7;
-    let t = 2;
-    let division= r/t ;
-    return console.log (division)
-}
-
-division()
+     }
